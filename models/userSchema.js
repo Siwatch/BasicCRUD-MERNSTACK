7554e42dac0c9d5fdca51c29f7fe3a5420625d2d
Siwatch Sritrakul 263 <siwatch.sritrakul@g.swu.ac.th>
2022-05-18 19:14:38 +0700

    Commit

---
+++ models/userSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    age:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        required:true
+    },
+    work:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    desc:{
+        type:String,
+        required:true
+    },
+    
+},
+{
+    timestamps:true
+});
+
+const users = new mongoose.model("users" , userSchema);
+
+module.exports = users;
